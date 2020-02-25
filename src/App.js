62a96100cfa320837d2527631289987df1c7ca1a
David Moran-Silva <moransilvadavid3@gmail.com>
2020-02-24 19:18:14 -0500

    mo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         My name is David. I am going to master reactgit add
+         My name is David. Im am going to master reactgit add
         </p>
         <a
           className="App-link"
