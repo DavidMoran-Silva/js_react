50cacbc9429cb3fc9cc2c2c64335249894d13ed8
David Moran-Silva <moransilvadavid3@gmail.com>
2020-02-24 22:23:34 -0500

    last commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         My name is David. Im am going to master reactgit add
+         My name is David. Im am going to master react
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/DavidMoran-Silva"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me out!
         </a>
       </header>
     </div>
