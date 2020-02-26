da446dd45a55c23eea6866ef8d3d973583931905
David Moran-Silva <moransilvadavid3@gmail.com>
2020-02-25 19:09:09 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Check me out!
+          Check me out!!
         </a>
       </header>
     </div>
