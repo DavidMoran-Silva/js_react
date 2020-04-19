024857bc44fcae6c49a9291ed4ef4bf61a38cdad
DavidMoran-Silva <moransilvadavid3@gmail.com>
2020-04-19 15:01:44 -0400

    Update App.js
    status update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import React from 'react';
-import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
+
 class App extends React.Component {
-//convert app.js into a class base comp bc using state
-
-constructor(props){
-  super(props)
-  this.state = {
-    title:"David Moran-Silva",
-    headerLinks: [
-      { title : 'Home', path: '/' },
-      { title : 'Details', path: '/details' },
-       {title : 'Personal', path: '/personal'}
-    ],
-    home: { 
-    title: 'Creativity',
-    subTitle:'Best projects',
-    text: 'check it out'
-    },
-
-  details:{
-    title: 'detailss'
-  },
-  personal:{
-    title: 'Of me'
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'David Moran-Silva',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'Be Creative',
+        subTitle: 'Projects',
+        text: 'Checkout my projects below'
+      },
+      about: {
+        title: 'Personal info'
+      },
+      contact: {
+        title: 'Chat?'
+      }
+    }
   }
 
-  }
-}
+  render() {
+    return (
+    <Router>
+        <Container className="p-0" fluid={true}>
+          
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>David Moran-Silva</Navbar.Brand>
 
-  render(){
-  return (
-     <Router>
-       
+             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+             <Navbar.Collapse id="navbar-toggle">
+                <Nav className="ml-auto">
+                   <Link className="nav-link" to="/">Home</Link>
+                   <Link className="nav-link" to="/about">About</Link>
+                   <Link className="nav-link" to="/contact">Contact</Link>
+                </Nav>
+             </Navbar.Collapse>
+          </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+        <Footer />
+
+        </Container>
      </Router>
     );
   }
