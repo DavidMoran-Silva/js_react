2817a65da7af701864f43acf00111c065ad775ed
DavidMoran-Silva <moransilvadavid3@gmail.com>
2020-04-19 15:28:29 -0400

    Update Footer.js
    update

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+function Footer(){
+     return(
+     
+        <Footer className = 'mt-5'>
+            <Container>
+                <Row>
+
+                </Row>
+            </Container>
+
+        </Footer>
+     );
+
+
+}
+export default Footer;
