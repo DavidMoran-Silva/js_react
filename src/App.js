eef1b8f18f586849849b6423f3fff41cac6d4e0c
DavidMoran-Silva <moransilvadavid3@gmail.com>
2020-04-19 01:50:31 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
 //convert app.js into a class base comp bc using state
 
 constructor(props){
   super(props)
   this.state = {
-    title:"David Moran-Silva"
+    title:"David Moran-Silva",
     headerLinks: [
       { title : 'Home', path: '/' },
       { title : 'Details', path: '/details' },
