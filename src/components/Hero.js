f6925d856677c49a604a10c015954b4b7d1d05db
DavidMoran-Silva <moransilvadavid3@gmail.com>
2020-04-19 18:31:52 -0400

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import Jumbroton from 'react-bootstrap/Jumbotron';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function Hero(){
+function Hero(props) {
 
-    return (
-        <Jumbroton>
-            <Container>
-                <Row>
-                    <Col>
-                    <h2>David Moran-Silva</h2>
+    return(
+        <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+            <Container fluid={true}>
+                <Row className="justify-content-center py-5">
+                    <Col md={8} sm={12}>
+                        { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
+                        { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
+                        { props.text && <h3 className="lead font-weight-light">{props.text}</h3> }
                     </Col>
                 </Row>
             </Container>
-        </Jumbroton>
+        </Jumbotron>
     );
-    
+
 }
-export default Hero
+
+
+export default Hero;
