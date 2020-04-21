eb63fadc67db2e7df3ee8809988dc593c2a8f652
DavidMoran-Silva <moransilvadavid3@gmail.com>
2020-04-20 23:26:38 -0400

    Update AboutPage.js
    Cover

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,7 +10,15 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-            <p>My paragraph that shows the different sites</p>
+            <p>My name is David Moran-Silva  and I am currently a full time student at Lehman College.</p>
+    
+            <p> My major is computer science and my curriculum has given me a solid technical foundation from which to build upon.</p>
+
+            <p> I furthermore have a passion for cybersecurity which motivates me to continually learn new skills via capture the flag and cyber war games as well as via personal study of important cyber concepts including networking and common exploits.</p>
+
+            <p> I have a working knowledge of Java through my classes and personal projects which give me a good understanding of programming fundamentals.</p>
+
+            <p> I believe I possess the motivation and aptitude to rapidly learn new skills and integrate myself into the team, which makes me an ideal intern candidate. I appreciate your time and look forward to hearing from you.</p>
             </Content>
             <Carousel/>
         </div>
